530d03fdf85cd7dd3849345d684c1543c068a9fb
David Helkowski <github@codechild.com>
2020-12-24 22:07:04 -0800

    Display read config in app

--- vidtest/vidtest/ViewController.h
+++ vidtest/vidtest/ViewController.h
@@ -6,6 +6,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#include "../vidtest2/ujsonin/ujsonin.h"
 
 @interface ViewController : UIViewController
 
--- vidtest/vidtest/ViewController.m
+++ vidtest/vidtest/ViewController.m
@@ -10,6 +10,13 @@
 
 @implementation ViewController
 
+-(ViewController *) init {
+    self = [super init];
+    ujsonin_init();
+    
+    return self;
+}
+
 - (void)viewDidLoad {
     NSFileManager *fileMan = [NSFileManager defaultManager];
     NSURL *sharedUrl = [fileMan containerURLForSecurityApplicationGroupIdentifier:@"group.com.dryark.vidtest"];
@@ -26,7 +33,35 @@
     [fileMan removeItemAtURL:destUrl error:&err];
     [fileMan copyItemAtURL:sourceUrl toURL: destUrl error:&err];
     
-    _blah.text = @"Hello World";
+    NSString *confPath = [sourceUrl absoluteString];
+    const char *cPath = [confPath UTF8String];
+    //NSLog( @"xxr config path %s", cPath );
+    FILE *fh = fopen( &cPath[7], "r" );
+    
+    fseek(fh, 0, SEEK_END);
+    long fsize = ftell(fh);
+    fseek(fh, 0, SEEK_SET);  /* same as rewind(f); */
+
+    char *string = malloc(fsize + 1);
+    fread(string, 1, fsize, fh);
+    
+    int err2;
+    node_hash *root = parse( (char *) string, (int) fsize, NULL, &err2 );
+    jnode *port = node_hash__get( root, "port", 4 );
+    jnode *ip = node_hash__get( root, "ip", 2 );
+    if( port->type == 2 && ip->type == 2 ) {
+        node_str *strOb = (node_str *) port;
+        node_str *ipOb = ( node_str * ) ip;
+        
+        char buffer[30];
+        sprintf(buffer,"%.*s:%.*s",ipOb->len,ipOb->str,strOb->len,strOb->str);
+        
+        NSString *str = [NSString stringWithUTF8String:buffer];
+        
+        _blah.text = str;
+    } else {
+        _blah.text = @"Config invalid";
+    }
     
     [super viewDidLoad];
     // Do any additional setup after loading the view.
