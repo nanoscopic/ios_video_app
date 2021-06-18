d175f3c3644d0b4b4e72b3dc07ebdc7b13f6d4c9
David Helkowski <github@codechild.com>
2021-06-17 18:56:33 -0700

    Enable start/stop from app
    Added access to the broadcast selection screen from within the app itself, so that it is easier to progamatically start video streaming. This makes it unnecessary to access the control center to do so.

--- vidstream/vidstream/SceneDelegate.m
+++ vidstream/vidstream/SceneDelegate.m
@@ -6,7 +6,7 @@
 //
 
 #import "SceneDelegate.h"
-
+#import "ViewController.h"
 @interface SceneDelegate ()
 
 @end
@@ -18,6 +18,14 @@
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    //UIWindow *window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds ];
+    UIWindowScene *winScene = (UIWindowScene *) scene;
+    UIWindow *window = [[UIWindow alloc] initWithWindowScene: winScene];
+    ViewController *view = [[ViewController alloc] init];
+    //application.windows[0].rootViewController = view;
+    window.rootViewController = view;
+    [window makeKeyAndVisible];
+    self.window = window;
 }
 
 
--- vidstream/vidstream/ViewController.m
+++ vidstream/vidstream/ViewController.m
@@ -2,12 +2,9 @@
 // Anti-Corruption License
 
 #import "ViewController.h"
+#import <ReplayKit/ReplayKit.h>
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UILabel *blah;
-@property (weak, nonatomic) IBOutlet UILabel *inputPort;
-@property (weak, nonatomic) IBOutlet UILabel *controlPort;
-
 @end
 
 @implementation ViewController
@@ -18,11 +15,30 @@
 }
 
 - (void)viewDidLoad {
-    _inputPort.text = @"Input port 8352";
-    _controlPort.text = @"Control port 8351";
-    _blah.text = @"";
-    
     [super viewDidLoad];
+    
+    UIView *view = [[UIView alloc] initWithFrame: UIScreen.mainScreen.bounds /*CGRectMake(0,0,320,300)*/];
+    UIColor *color = [UIColor colorWithRed:246/255.f green:0/255.f blue:0/255.f alpha:1.0];
+    view.backgroundColor = color;
+    
+    UIColor *color2 = [UIColor colorWithRed:0/255.f green:200/255.f blue:0/255.f alpha:1.0];
+    RPSystemBroadcastPickerView *picker = [[RPSystemBroadcastPickerView alloc] initWithFrame:CGRectMake(0,40,320,200)];
+    
+    NSString *bi = [[NSBundle mainBundle] bundleIdentifier];
+    NSString *ebi = [bi stringByAppendingString:@".extension"];
+    
+    picker.preferredExtension = ebi;//@"com.dryark.vidstream.extension";
+    picker.backgroundColor = color2;
+    
+    UIButton *btn = picker.subviews[0];
+    [btn setTitle:@"  Broadcast Selector" forState:UIControlStateNormal];
+    UIImage *playImg = [UIImage systemImageNamed: @"play"];
+    [btn setImage: playImg forState:UIControlStateNormal];
+    [view addSubview: picker];
+    
+    [self.view addSubview: view];
+    
+    
 }
 
 @end
